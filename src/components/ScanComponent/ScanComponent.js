6bca85806fb97bfe93975539b5ba8a5620e2167a
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-07-19 17:00:07 +0300

    rewrite all logic ScanComponent + css

--- src/components/ScanComponent/ScanComponent.js
+++ src/components/ScanComponent/ScanComponent.js
@@ -1,37 +1,113 @@
-import React, {useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import styles from "./ScanComponent.module.css"
 import axios from "axios";
+import Webcam from "react-webcam";
+import {UPLOAD_URL} from "../../backPathes";
+import {videoConstraints} from "../../cameraResolution";
 
 const ScanComponent = (props) => {
 
-    const [scannedObject, setScannedObject] = useState()
+    const [scannedObject, setScannedObject] = useState(null);
+    const webcamRef = useRef();
+    const [image, setImage] = useState(null);
     const {setResultValue} = props;
 
-    const getScanned = () => {
-        axios.get("http://127.0.0.1:8000/")
-            .then(response => {
-                setResultValue(response.data)
-                const data = response.data.map(obj => {
-                    return (
-                        <div key={obj.id}>
-                            {obj.name} - {obj.price}$
-                        </div>
-                    )
-                });
-                console.log(data)
-                setScannedObject(data)
-            })
-            .catch(error => console.log(error))
+    useEffect(() => {
+        console.log(scannedObject)
+    }, [scannedObject])
+
+    const handleCaptureScreenshot = useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+            setImage(imageSrc)
+        }, [webcamRef]
+    );
+
+    const resetCapture = () => {
+        setImage(null);
+        setResultValue(null)
+        setScannedObject(null)
+    }
+
+
+    const sendData = () => {
+        const formData = new FormData();
+        const blob = dataURItoBlob(image);
+        // Update the formData object
+        formData.append("image", blob, "test.jpg");
+        // Details of the uploaded file
+        axios.post(UPLOAD_URL, formData, {
+            headers: {
+                'Content-Type': "multipart/form-data"
+            }
+        }).then(response => {
+            const resultProducts = response.data;
+            setScannedObject(resultProducts)
+            setResultValue(resultProducts)
+        }).catch(e => console.log(e));
+    }
+
+    const dataURItoBlob = (dataURI) => {
+        let binary = atob(dataURI.split(',')[1]);
+        let array = [];
+        for (let i = 0; i < binary.length; i++) {
+            array.push(binary.charCodeAt(i));
+        }
+        return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
     }
 
     return (
         <div className={styles.ProductList}>
             <div className={styles.ResultWindow}>
-                {scannedObject ? <div>{scannedObject}</div> : <p>Нажмите, чтобы сканировать....</p>}
+                {
+                    scannedObject && <div>
+                        {
+                            scannedObject.products &&
+                            scannedObject.products.length > 0 ?
+                                (
+                                    <h2>Scanned list</h2> && scannedObject.products.map((product, index) => {
+                                        return (
+                                            <div key={index}>
+                                                <p>Name: {product.name}</p>
+                                                <p>Price: {product.price}</p>
+                                            </div>
+                                        )
+                                    })
+                                )
+                                : <div>
+                                    No matches found =(
+                                </div>
+                        }
+                    </div>
+                }
+                {
+                    !scannedObject && (image ? <img src={image} alt="screenshot"/>
+                        : <Webcam screenshotFormat="image/jpeg"
+                                  ref={webcamRef}
+                                  videoConstraints={videoConstraints}
+                                  width={480}
+                                  height={480}
+                                  audio={false}/>)
+                }
             </div>
-            <button onClick={getScanned} className="btn btn-primary btn-lg">
-                SCAN
-            </button>
+            {
+                !image &&
+                <button onClick={handleCaptureScreenshot} className={`btn btn-outline-primary ${styles.Button}`}>
+                    SCAN
+                </button>
+            }
+            {
+                image &&
+                <>
+                    <button onClick={sendData} className={`btn btn-outline-success ${styles.Button}`}>
+                        SEND PHOTO
+                    </button>
+                    <button onClick={resetCapture} className={`btn btn-outline-danger ${styles.Button}`}>
+                        RESET
+                    </button>
+                </>
+            }
+
         </div>
     )
 }
