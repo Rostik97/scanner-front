22eee2cda60c27cbbb8557fba8e077ca009f6f58
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-08-21 13:09:22 +0300

    moved WebCum component to another package add verify auth logic

---
+++ src/components/WebCum/WebCum.js
@@ -0,0 +1,30 @@
+import styles from "./WebCum.module.css"
+import React, {useState} from "react";
+import {useAuth} from "../../useAuth";
+import {useNavigate} from "react-router-dom";
+import Preview from "../Preview/Preview";
+import ScanComponent from "../Scan/ScanComponent";
+import TotalPrice from "../TotalPrice/TotalPrice";
+
+const WebCum = () => {
+    const [showPreview, setShowPreview] = useState(true);
+    const [resultValue, setResultValue] = useState(null);
+    const {isAuth, token} = useAuth();
+    const navigate = useNavigate();
+    const closePreview = () => setShowPreview(false);
+
+    if (!isAuth) {
+        navigate("/login")
+    }
+
+
+    return (
+        <div className={styles.Content}>
+            <Preview show={showPreview} handleClose={closePreview}/> :
+            <ScanComponent setResultValue={setResultValue} token={token}/>
+            <TotalPrice resultValue={resultValue}/>
+        </div>
+    )
+}
+
+export default WebCum;
