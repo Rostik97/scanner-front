b32e2318d35067ab8bce2ae1511312db59963677
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-08-21 13:17:22 +0300

    Add routing + auth to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
 import './App.css';
-import TotalPrice from "./TotalPriceComponent/TotalPrice";
-import React, {useState} from "react";
-import ScanComponent from "./ScanComponent/ScanComponent";
+import React from "react";
+import {Navigate, Route, Routes} from "react-router-dom";
+import WebCum from "./WebCum/WebCum";
+import Authorization from "./Authorization/Authorization";
+import {useAuth} from "../useAuth";
+import Registration from "./Registration/Registration";
 
 function App() {
 
-    const [resultValue, setResultValue] = useState(null)
+    const {isAuth} = useAuth();
 
+    if (!isAuth) {
+        return (
+            <Routes>
+                <Route path="/login" element={<Authorization/>}/>
+                <Route path="/register" element={<Registration/>}/>
+                <Route path="/*" element={<Navigate replace to="/login"/>}/>
+            </Routes>
+        )
+    }
 
     return (
         <div className="App">
-            <div className="Content">
-                <ScanComponent setResultValue={setResultValue}/>
-                <TotalPrice resultValue={resultValue}/>
-            </div>
+            <Routes>
+                <Route path="/" element={<WebCum/>}/>
+                <Route path="/login" element={<Authorization/>}/>
+                <Route path="/register" element={<Registration/>}/>
+            </Routes>
         </div>
     );
 }
