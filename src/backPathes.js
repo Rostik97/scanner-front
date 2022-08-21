d9d5bd83bec4eb313a1996aaf3fb2b457e85bed7
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-08-21 13:09:58 +0300

    add new constants (back paths)

--- src/backPathes.js
+++ src/backPathes.js
@@ -1,2 +1,5 @@
 // export const UPLOAD_URL = "http://127.0.0.1:8000/upload";
-export const UPLOAD_URL = "http://46.118.10.196/products/";
+// export const UPLOAD_URL = "https://back.smartscanapp.one/scan/";
+export const UPLOAD_URL = "https://api.smartscanapp.one/scan/";
+export const REGISTER_URL = "https://api.smartscanapp.one/register/";
+export const AUTH_URL = "https://api.smartscanapp.one/api/token/";
