6d4cd1062e8bf5ecd8acb8ade6193624fe912afd
Alekseev Rostislav <rostislav.alekseev@privatbank.ua>
2022-10-24 21:54:56 +0300

    rewrite App component with Layout

--- src/backPathes.js
+++ src/backPathes.js
@@ -1,5 +1,6 @@
 // export const UPLOAD_URL = "http://127.0.0.1:8000/upload";
 // export const UPLOAD_URL = "https://back.smartscanapp.one/scan/";
-export const UPLOAD_URL = "https://api.smartscanapp.one/scan/";
+export const PRODUCTS_URL = "https://api.smartscanapp.one/scan/";
 export const REGISTER_URL = "https://api.smartscanapp.one/register/";
 export const AUTH_URL = "https://api.smartscanapp.one/api/token/";
+export const FACE_SCAN_URL = "https://api.smartscanapp.one/facescan/";
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import WebCum from "./WebCum/WebCum";
 import Authorization from "./Authorization/Authorization";
 import {useAuth} from "../useAuth";
 import Registration from "./Registration/Registration";
+import MainPage from "./MainPage/MainPage";
+import Personal from "./PersonalPage/Personal";
+import Layout from "./Layout/Layout";
 
 function App() {
 
@@ -17,13 +20,19 @@ function App() {
                 <Route path="/register" element={<Registration/>}/>
                 <Route path="/*" element={<Navigate replace to="/login"/>}/>
             </Routes>
-        )
+        );
     }
 
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<WebCum/>}/>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="/personal" element={<Personal/>}/>
+                    <Route path="/products" element={<WebCum/>}/>
+                    <Route path="/" element={<Navigate replace to="/main"/>}/>
+                    <Route path="/*" element={<Navigate replace to="/login"/>}/>
+                </Route>
+                <Route path="/main" element={<MainPage/>}/>
                 <Route path="/login" element={<Authorization/>}/>
                 <Route path="/register" element={<Registration/>}/>
             </Routes>
