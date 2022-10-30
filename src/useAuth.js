bf96bdfb677761ba5162929dad75c5c41b0f076a
Алексеев Ростислав Сергеевич <rostislav.alekseev@privatbank.ua>
2022-10-30 22:49:23 +0200

    update useAuth hook

--- src/useAuth.js
+++ src/useAuth.js
@@ -1,19 +1,26 @@
-import {useSelector} from "react-redux";
-
+import {useDispatch, useSelector} from "react-redux";
+import {removeUser} from "./store/userSlice";
+import {useNavigate} from "react-router-dom";
 
 export function useAuth() {
-    const {email, token, id, userName} = useSelector(state => state.user);
+    const {email, token, userName} = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const isAuth = () => {
-        //TODO
         return token ? true : localStorage.getItem("token");
+    }
+
+    const logOut = () => {
+        dispatch(removeUser());
+        navigate("/");
     }
 
     return {
         isAuth,
+        logOut,
         email,
         userName,
-        token: localStorage.getItem("token"),
-        id
+        token
     };
 }
