26227d33f1bccbb7bc58c437d4c68d1064735f97
Алексеев Ростислав Сергеевич <rostislav.alekseev@privatbank.ua>
2022-10-30 22:52:17 +0200

    optimized routing logic because of private routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,32 +3,22 @@ import React from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
 import WebCum from "./WebCum/WebCum";
 import Authorization from "./Authorization/Authorization";
-import {useAuth} from "../useAuth";
 import Registration from "./Registration/Registration";
 import MainPage from "./MainPage/MainPage";
 import Personal from "./PersonalPage/Personal";
 import Layout from "./Layout/Layout";
+import {RequireAuth} from "../hoc/RequireAuth";
 
 function App() {
-
-    const {isAuth} = useAuth();
-
-    if (!isAuth()) {
-        return (
-            <Routes>
-                <Route path="/login" element={<Authorization/>}/>
-                <Route path="/register" element={<Registration/>}/>
-                <Route path="/*" element={<Navigate replace to="/login"/>}/>
-            </Routes>
-        );
-    }
 
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<Layout/>}>
-                    <Route path="/personal" element={<Personal/>}/>
-                    <Route path="/products" element={<WebCum/>}/>
+                    <Route element={<RequireAuth/>}>
+                        <Route path="/personal" element={<Personal/>}/>
+                        <Route path="/products" element={<WebCum/>}/>
+                    </Route>
                     <Route path="/" element={<Navigate replace to="/main"/>}/>
                     <Route path="/*" element={<Navigate replace to="/login"/>}/>
                 </Route>
