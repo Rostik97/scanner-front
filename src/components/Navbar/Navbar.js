b0f5e5f8bd039faea811954e085f74647e29c481
Алексеев Ростислав Сергеевич <rostislav.alekseev@privatbank.ua>
2022-11-05 22:19:25 +0200

    add logic to logout img Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,32 +2,30 @@ import styles from "./Navbar.module.css"
 import {Link, NavLink} from "react-router-dom";
 import Logo from "./image.svg";
 import React from "react";
-// import LogOutIcon from './log_out.svg'
-import { ReactComponent as LogOutIcon } from './log_out.svg';
+import {ReactComponent as LogOutIcon} from './log_out.svg';
+import {useAuth} from "../../useAuth";
 
 const Navbar = () => {
-    return (
-        <div className={styles.Container}>
-            <Link to="/">
-                <img src={Logo} alt="Logo"/>
-            </Link>
-            <div className={styles.Menu}>
-                <NavLink to="/">
-                    <div>HOME</div>
-                </NavLink>
-                <NavLink to="/personal">
-                    <div>PERSONAL</div>
-                </NavLink>
-                <NavLink to="/products">
-                    <div>PRODUCTS</div>
-                </NavLink>
-                {/*<img className={styles.LogOutIcon} src={LogOutIcon} alt="LogOut"/>*/}
-                <div className={styles.LogOutIcon}>
-                    <LogOutIcon fill="black" width='25' height='25'/>
-                </div>
-            </div>
+
+    const {logOut} = useAuth();
+
+    return (<div className={styles.Container}>
+        <Link to="/">
+            <img src={Logo} alt="Logo"/>
+        </Link>
+        <div className={styles.Menu}>
+            <NavLink to="/">
+                <div>HOME</div>
+            </NavLink>
+            <NavLink to="/personal">
+                <div>PERSONAL</div>
+            </NavLink>
+            <NavLink to="/products">
+                <div>PRODUCTS</div>
+            </NavLink>
+            <LogOutIcon onClick={logOut} className={styles.LogOutIcon}/>
         </div>
-    )
+    </div>)
 }
 
 export default Navbar;
